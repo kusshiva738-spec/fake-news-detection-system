156cda4ecda3c0a1f1eaa8c248218a24a5ae4d87
kusshiva738-spec <kusshiva738@gmail.com>
2026-03-17 21:30:39 +0530

    initial commit

---
+++ authority_emails.py
@@ -0,0 +1,10 @@
+authority_emails = {
+
+    "kanpur": "kusshiva738@gmail.com",
+    "lucknow": "dmlucknow@nic.in",
+    "delhi": "dcpdelhi@nic.in",
+    "mumbai": "collectormumbai@nic.in",
+    "bhopal": "collectorbhopal@nic.in",
+    "indore": "collectorindore@nic.in"
+
+}
